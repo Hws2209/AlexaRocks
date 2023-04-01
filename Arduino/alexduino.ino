fcfc3b3fd2142564bba52fe49a1ff8b23046cb9a
Hws2209 <wansing03@gmail.com>
2023-04-01 21:20:29 +0800

    Update alexduino.ino

--- Arduino/alexduino.ino
+++ Arduino/alexduino.ino
@@ -101,6 +101,16 @@ void detectColour()
 
 void setupColour()
 {
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);  
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  
+  pinMode(sensorOut, INPUT);
+  
+  // Setting frequency scaling to 20%
+  digitalWrite(S0,HIGH);  
+  digitalWrite(S1,LOW);
 }
 
 void setup() {
