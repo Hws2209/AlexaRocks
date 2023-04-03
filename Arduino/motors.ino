32b0e9499d94552bb8b665803702d53eab5cf0ca
Hws2209 <wansing03@gmail.com>
2023-04-03 22:00:16 +0800

    Update motors.ino

--- Arduino/motors.ino
+++ Arduino/motors.ino
@@ -7,7 +7,7 @@ void setupMotors()
   //        A1IN - Pin 5, PD5, OC0B
   //        A2IN - Pin 6, PD6, OC0A
   //        B1IN - Pin 10, PB2, OC1B
-  //        B2In - pIN 11, PB3, OC2A
+  //        B2IN - Pin 11, PB3, OC2A
 }
 
 // Start the PWM for Alex's motors.
