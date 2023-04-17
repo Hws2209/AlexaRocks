a78a2ad48627fa29d75eddce0df482f2443956bb
Hws2209 <wansing03@gmail.com>
2023-04-17 19:46:04 +0800

    Update colour&ultra.ino

--- realduino/fakeduino/colour&ultra.ino
+++ realduino/fakeduino/colour&ultra.ino
@@ -1,3 +1,17 @@
+void setupColour()
+{
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+
+  pinMode(sensorOut, INPUT);
+
+  // Setting frequency scaling to 20%
+  digitalWrite(S0, HIGH);
+  digitalWrite(S1, LOW);
+}
+
 double calcHue()
 {
   double max = rgb[0];
@@ -90,18 +104,4 @@ void ultrasonic() {
 
   distance = duration / 58.0;
 
-}
-
-void setupColour()
-{
-  pinMode(S0, OUTPUT);
-  pinMode(S1, OUTPUT);
-  pinMode(S2, OUTPUT);
-  pinMode(S3, OUTPUT);
-
-  pinMode(sensorOut, INPUT);
-
-  // Setting frequency scaling to 20%
-  digitalWrite(S0, HIGH);
-  digitalWrite(S1, LOW);
 }
