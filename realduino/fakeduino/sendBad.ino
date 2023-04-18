5ea87341897f223bb0a92c57141533be6055fb9a
Hws2209 <wansing03@gmail.com>
2023-04-18 20:05:34 +0800

    Update sendBad.ino

--- realduino/fakeduino/sendBad.ino
+++ realduino/fakeduino/sendBad.ino
@@ -5,33 +5,31 @@ void sendBadResponse()
   badResponse.command = RESP_BAD_RESPONSE;
   sendResponse(&badResponse);
 }
+
 void sendBadPacket()
 {
   // Tell the Pi that it sent us a packet with a bad
   // magic number.
-
   TPacket badPacket;
   badPacket.packetType = PACKET_TYPE_ERROR;
   badPacket.command = RESP_BAD_PACKET;
   sendResponse(&badPacket);
-
 }
+
 void sendBadCommand()
 {
   // Tell the Pi that we don't understand its
   // command sent to us.
-
   TPacket badCommand;
   badCommand.packetType = PACKET_TYPE_ERROR;
   badCommand.command = RESP_BAD_COMMAND;
   sendResponse(&badCommand);
-
 }
+
 void sendBadChecksum()
 {
   // Tell the Pi that it sent us a packet with a bad
   // checksum.
-
   TPacket badChecksum;
   badChecksum.packetType = PACKET_TYPE_ERROR;
   badChecksum.command = RESP_BAD_CHECKSUM;
